e46ca2f14e68e6b72d391d657920a2df47c7be09
Dereje Desta <ddesta777@gmail.com>
2022-04-01 17:15:40 +0300

    minor fix for 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-from fabric.api import local, cd, put, env, run
+from fabric.api import local, cd, put, env, run, sudo
 from datetime import datetime
 from os import path
+
+env.hosts = ['3.239.74.200', '3.238.233.55']
 
 
 def do_pack():
@@ -16,21 +18,20 @@ def do_pack():
 
 def do_deploy(archive_path):
     """distributes an archive to web servers"""
-    env.hosts = ['3.239.74.200', '3.238.233.55']
+    archive_withoutext = path.splitext(path.basename(archive_path))[0]
     if (not path.exists(archive_path)):
         return False
     with cd('/tmp'):
-        upload = put(archive_path, archive_path)
-    archive_withoutext = path.splitext(path.basename(archive_path))[0]
-    run('mkdir -p /data/web_static/releases/{}'.format(archive_withoutext))
-    run('tar -xzf /tmp/{0}.tgz -C /data/web_static/releases/{0}/'.format(
+        upload = put(archive_path, archive_withoutext + '.tgz')
+    sudo('mkdir -p /data/web_static/releases/{}'.format(archive_withoutext))
+    sudo('tar -xzf /tmp/{0}.tgz -C /data/web_static/releases/{0}/'.format(
          archive_withoutext))
-    run('rm /tmp/{}.tgz'.format(archive_withoutext))
-    run('mv /data/web_static/releases/{0}/web_static/* /data/web_static/\
-        releases/{0}/'.format(archive_withoutext))
-    run('rm -rf /data/web_static/releases/{}/web_static'.format(
+    sudo('rm /tmp/{}.tgz'.format(archive_withoutext))
+    sudo('mv /data/web_static/releases/{0}/web_static/* \
+    /data/web_static/releases/{0}/'.format(archive_withoutext))
+    sudo('rm -rf /data/web_static/releases/{}/web_static'.format(
         archive_withoutext))
-    run('rm -rf /data/web_static/current')
-    run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(
+    sudo('rm -rf /data/web_static/current')
+    sudo('ln -s /data/web_static/releases/{} /data/web_static/current'.format(
         archive_withoutext))
     return upload.succeeded
